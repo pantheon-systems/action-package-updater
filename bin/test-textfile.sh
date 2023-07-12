04e472a034b2d4a9726e8d50066d1f0e7d270b7f
Chris Reynolds <chris@jazzsequence.com>
2023-07-12 09:02:32 -0600

    add a test for plain text files e.g. PHP_EXTENSION_VERSIONS

---
+++ bin/test-textfile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eou pipefail
+
+filename="PHP_EXTENSION_VERSIONS"
+
+# Read the contents of the file
+file_contents=$(cat "$filename")
+
+# Enable case-insensitive matching
+shopt -s nocasematch
+
+# Regular expression pattern for standard numeric format allowing RC or Beta releases
+version_pattern='^([0-9]+\.){1,2}[0-9]+(-RC[0-9]+|-Beta[0-9]+)?$'
+
+# Initialize a flag variable to track validation status
+valid_versions=true
+
+# Iterate over each line in the file
+while IFS= read -r line; do
+    # Check if the line matches the version pattern
+    if [[ ! $line =~ $version_pattern ]]; then
+        echo "Invalid version: $line"
+        valid_versions=false
+    fi
+done <<< "$file_contents"
+
+# Exit with an error code if any version comparison is invalid
+if ! "$valid_versions"; then
+    echo "One or more versions are invalid."
+    exit 1
+fi
